1e5478dd7a0eeeeceb17a2b9bf42b3b97e3df2bc
Alexander Rysenko <rysenko@gmail.com>
2015-10-24 20:00:35 +0300

    Add basic recursive support

--- test/etcd.spec.js
+++ test/etcd.spec.js
@@ -5,13 +5,31 @@ describe('Etcd mock', function() {
   before(function() {
     this.client = new Etcd();
   });
-  it('should set', function(done) {
-    this.client.set('/root', 'value', done);
+  it('should set sub', function(done) {
+    this.client.set('/root/child/sub', 'value', done);
   });
-  it('should get', function(done) {
-    this.client.get('/root', (err, result) => {
+  it('should get child', function(done) {
+    this.client.get('/root/child/sub', (err, result) => {
       assert.ifError(err);
       assert.equal(result.node.value, 'value');
+      done();
+    });
+  });
+  it('should get root', function(done) {
+    this.client.get('/root', (err, result) => {
+      assert.ifError(err);
+      assert.equal(result.node.nodes.length, 1);
+      assert.equal(result.node.nodes[0].key, '/root/child');
+      assert(!result.node.nodes[0].nodes);
+      done();
+    });
+  });
+  it('should get root recursive', function(done) {
+    this.client.get('/root', {recursive: true}, (err, result) => {
+      assert.ifError(err);
+      assert.equal(result.node.nodes.length, 1);
+      assert.equal(result.node.nodes[0].key, '/root/child');
+      assert.equal(result.node.nodes[0].nodes[0].key, '/root/child/sub');
       done();
     });
   });
